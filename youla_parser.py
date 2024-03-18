8f3739ad48a7761521a7a700204ab405bc988c25
Th3VaultDweller <aztec242@yandex.ru>
2024-03-19 00:38:22 +0300

    Added browser options

--- youla_parser.py
+++ youla_parser.py
@@ -13,3 +13,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from webdriver_manager.chrome import ChromeDriverManager
+
+# прописываем опции для запуска браузера
+option = Options()
+option = webdriver.ChromeOptions()
+# option.add_argument("--incognito")  # режим инкогнито
+option.add_argument("--disable-infobars")  # отключение всплывающих окон
+option.add_argument("--start-maximized")  # включение полноэкранного режима
+option.add_argument(
+    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+)
